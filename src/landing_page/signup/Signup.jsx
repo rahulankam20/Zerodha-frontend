4d17e727306c49d523bfbe0f29d442d506fef391
Rahul Ankam <rahulankam1234@gmail.com>
2025-06-21 01:14:46 +0530

    corrected url

--- src/landing_page/signup/Signup.jsx
+++ src/landing_page/signup/Signup.jsx
@@ -16,7 +16,7 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch('https://zerodha-backend-viyu.onrender.com/api/auth/signin/api/auth/signup', {
+      const res = await fetch('https://zerodha-backend-viyu.onrender.com/api/auth/signup', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(form),
